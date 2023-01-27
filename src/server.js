4d524e985f8ecee9a7f686fe5a56134d5f07f5ec
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2023-01-27 15:24:46 +0100

    db connection

--- src/server.js
+++ src/server.js
@@ -13,9 +13,9 @@ server.use(express.json());
 
 server.use("/feedback", feedbackRouter);
 
-const process =
-    "mongodb+srv://Francescomicunco:8aP7DPMC2Bk9eU7X@cluster0.wlpz7.mongodb.net/algorabfeedback?retryWrites=true&w=majority";
-mongoose.connect(process);
+const process = process.env.MONGO_CONNECTION_STRING;
+
+mongoose.connect(`${process}`);
 mongoose.connection.on("connected", () => {
     server.listen(port, () => {
         console.log("server connected on port ", port);
